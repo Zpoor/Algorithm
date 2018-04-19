b273512fcb32c2791cd894cb6fe78ed59920eb8d
Zpoor <295397246@qq.com>
2018-04-19 16:34:53 +0800

    反转字符串
    补充了临时变量法，和栈结构法。话说debug真是个好东西，帮助我理解算法。

--- src/cn/zpoor/basics/ReverseOfString.java
+++ src/cn/zpoor/basics/ReverseOfString.java
@@ -1,4 +1,7 @@
 package cn.zpoor.basics;
+
+import java.util.Stack;
+
 /*
  * 字符串反转常用的几种实现方式
  * 
@@ -12,6 +15,8 @@ public class ReverseOfString {
 		System.out.println(reverse2("Hello2"));
 		System.out.println(reverse3("Hello3"));
 		System.out.println(reverse4("Hello4"));
+		System.out.println(reverse5("Hello5"));
+		System.out.println(reverse6("Hello6"));
 	}
 	
 	//第一种方法：递归
@@ -50,13 +55,31 @@ public class ReverseOfString {
 		return new StringBuffer(s).reverse().toString();//不想解释了，太偷懒了
 	}
 	
-	//第五种方法：临时变量法
+	//第五种方法：临时变量法 交换两头数据
 	public static String reverse5(String s) {
-		return s;
+		char[] arr = s.toCharArray();//字符串转char数组
+		int length = s.length()-1;//得到字符串的长度-1
+		int halfLength = length/2;//得到一般的字符串长度
+		for(int i = 0;i<=halfLength;i++) {//交换两头数组元素，很巧妙啊，小老板
+			char temp = arr[i];//最前面定义为临时变量
+			arr[i] = arr[length-i];//最后面和最前面交换
+			arr[length-i] = temp;//临时变量（最前面的值）和最后面的值交换
+		}
+		return new String(arr);//返回处理之后的字符串
 	}
 	
-	//第六种方法：栈方法
+	//第六种方法：栈方法(后进先出，小老板牛逼啊)
+	
 	public static String reverse6(String s) {
-		return s;
+		char[] str = s.toCharArray();//字符串转char数组
+		Stack<Character> stack = new Stack<>();//定义栈结构
+		for(int i = 0;i<str.length;i++) {
+			stack.push(str[i]);//压栈操作
+		}
+		String reverse = "";//空接受字符串
+		for(int i = 0;i<str.length;i++) {
+			reverse += stack.pop();//出栈（先进后出，所以最后进入的是5，然后最先出来的是5）
+		}
+		return reverse;
 	}
 }
